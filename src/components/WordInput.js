eed275044d001a16271b8b29b63de5a732dbe9b0
gautam <gautampal560@gmail.com>
2022-10-11 21:47:12 +0530

    update

--- src/components/WordInput.js
+++ src/components/WordInput.js
@@ -1,10 +1,18 @@
 import React from 'react';
 
-export const WordInput = () => {
+export const WordInput = (props) => {
+
+    function handleOnChange()
+    {
+        
+        props.setWI(document.getElementById("word-input").value);
+    }    
+    
+
     return (
         <>   
             <label htmlFor="word-input">Enter word to be highlighted here</label>
-            <input id="word-input" type={"text"}/>
+            <input id="word-input" type={"text"} onChange={handleOnChange} />
         </>
     )
 }
